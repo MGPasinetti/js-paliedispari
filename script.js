3d051c06d3e817f3a56f18d625eaeb46b3caff74
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-01 16:48:15 +0100

    making pari e dispari exercise

--- script.js
+++ script.js
@@ -4,39 +4,30 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-const userWord = prompt(`Inserisci una parola:`);
+// const userWord = prompt(`Inserisci una parola:`);
 
-console.log(userWord);
+// console.log(userWord);
 
-console.log(reverse(userWord));
-
-
-if (isPalindrome(userWord, reverse(userWord))) {
-    alert(`La parola inserita è PALINDROMA`);
-} else {
-    alert(`La parola inserita non è palindroma`);
-}
-
-function isPalindrome(word, wordReverse) {
-    if (word === wordReverse) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function reverse(s){
-    return [...s].reverse().join("");
-}
+// console.log(reverse(userWord));
 
 
+// if (isPalindrome(userWord, reverse(userWord))) {
+//     alert(`La parola inserita è PALINDROMA`);
+// } else {
+//     alert(`La parola inserita non è palindroma`);
+// }
 
+// function isPalindrome(word, wordReverse) {
+//     if (word === wordReverse) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-
-
-
-
-
+// function reverse(s){
+//     return [...s].reverse().join("");
+// }
 
 
 
@@ -48,3 +39,10 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+const evenOrOdd = prompt(`Pari o dispari?`);
+const userNum = parseInt(prompt(`Inserisci un numero da 1 a 5:`))
+
+const pcNumber = Math.floor(Math.random() * 5) + 1;
+
+

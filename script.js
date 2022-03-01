49da1c5663c620e36507bc22734a4b716c8fb363
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-01 15:21:02 +0100

    making es. palindroma

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+PALINDROMA:
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+let userWord = prompt(`Inserisci una parola:`);
+
+let arrWordSpread = [...userWord];
+
+console.log(arrWordSpread);
+
+// function isPalindrome(word) {
+//     if () {
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+PARI E DISPARI:
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
